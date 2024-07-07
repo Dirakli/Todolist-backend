37d36917127f72623413bacf76380c4fbe93d3ff
Your Name <dgebuadzeirakli28@gmail.com>
2024-07-08 01:43:22 +0400

    Add CORS

--- Task/Program.cs
+++ Task/Program.cs
@@ -19,6 +19,15 @@ builder.Services.AddScoped<IAssignmentRepository>(provider =>
 
 builder.Services.AddScoped<IAssignmentService, AssignmentService>();
 
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://localhost:4200")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -33,6 +42,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Use CORS
+app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
